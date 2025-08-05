98170ff599d298d2cb5338a0a3d6649c64d5d189
Jari Ahonen <jari.n.ahonen@gmail.com>
2025-08-05 14:46:07 +0300

    error classes

--- src/handlers/handlerValidateChirp.ts
+++ src/handlers/handlerValidateChirp.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { respondWithJSON } from "./utils.js";
+import { BadRequestError } from "../middleware/errorHandler.js";
 
 export async function handlerValidateChirp(req: Request, res: Response) {
   type parameters = {
@@ -9,7 +10,7 @@ export async function handlerValidateChirp(req: Request, res: Response) {
   const params: parameters = req.body;
 
   if (params.body.length > 140) {
-    throw new Error("Chirp is too long");
+    throw new BadRequestError("Chirp is too long. Max length is 140")
   }
 
   let payload = params.body.replaceAll(/kerfuffle|sharbert|fornax/gi, "****")
--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,13 +1,57 @@
 import { NextFunction, Request, Response } from "express";
 import { respondWithError } from "../handlers/utils.js";
 
+
+// Error 400
+export class BadRequestError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+// Error 401
+export class UnauthorizedError extends Error {
+    constructor(message: string) {
+        super(message)
+    }
+}
+
+// Error 403
+export class ForbiddenError extends Error {
+    constructor(message: string) {
+        super(message)
+    }
+}
+
+// Error 404
+export class NotFoundError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
 export function errorHandler(
     err: Error,
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-    const msg = "Something went wrong on our end"
+    const msg = err.message
     console.log(msg)
+
+    // Find the correct error code based on the error class
+    if (err instanceof BadRequestError) {
+        return respondWithError(res, 400, msg);
+    }
+    if (err instanceof UnauthorizedError) {
+        return respondWithError(res, 401, msg);
+    }
+    if (err instanceof ForbiddenError) {
+        return respondWithError(res, 403, msg);
+    }
+    if (err instanceof NotFoundError) {
+        return respondWithError(res, 404, msg);
+    }
+
     respondWithError(res, 500, msg)
 }
