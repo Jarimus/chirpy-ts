3a9cb332e60528688a475676f8239746265da39d
Jari Ahonen <jari.n.ahonen@gmail.com>
2025-08-13 20:32:47 +0300

    api/chirps - query param to sort results

--- src/handlers/handlerGetChirps.ts
+++ src/handlers/handlerGetChirps.ts
@@ -4,6 +4,13 @@ import { respondWithJSON } from "./utils.js";
 
 export async function handlerGetChirps(req:Request, res: Response) {
 
+    // Get sorting style from query parameter
+    let sort = "asc";
+    const sortQuery = req.query.sort;
+    if (sortQuery === "desc") {
+        sort = sortQuery
+    }
+
     // Check for a query to look for chirps by a specific author
     const authorId = req.query.authorId;
     if (typeof authorId === "string") {
@@ -12,10 +19,22 @@ export async function handlerGetChirps(req:Request, res: Response) {
             respondWithJSON(res, 200, { body: "No chirps."});
             return
         }
+        if (sort === "desc") {
+            chirps.sort((a, b) => {
+                return b.createdAt.getTime() - a.createdAt.getTime()
+            })
+        }
         respondWithJSON(res, 200, chirps)
+        return
     }
 
     const chirps = await getChirps();
+
+    if (sort === "desc") {
+        chirps.sort((a, b) => {
+            return b.createdAt.getTime() - a.createdAt.getTime()
+        })
+    }
 
     if (chirps.length === 0) {
         respondWithJSON(res, 200, {
